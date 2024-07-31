0a50572c3f3aeea158e0b3ccdb949efe685a8613
Ableducation <training@thinnkware.com>
2024-07-31 16:31:44 +0530

    Do all chnges

--- lms/settings.py
+++ lms/settings.py
@@ -162,7 +162,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
